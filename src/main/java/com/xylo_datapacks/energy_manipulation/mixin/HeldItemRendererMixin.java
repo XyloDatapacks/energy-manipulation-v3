7ba7d737a1fa4f2d184250cc4a848460c4b90465
XyloDatapacks <xylo.datapacks@gmail.com>
2024-07-29 17:06:39 +0200

    idk kinda fixed the spell book first person animation. still has to really fix the thing, but at least does not offset anymore. it just put it in the crossbow position. so i have to use the right matrices.translate

--- src/main/java/com/xylo_datapacks/energy_manipulation/mixin/HeldItemRendererMixin.java
+++ src/main/java/com/xylo_datapacks/energy_manipulation/mixin/HeldItemRendererMixin.java
@@ -1,26 +1,43 @@
 package com.xylo_datapacks.energy_manipulation.mixin;
 
 
+import com.xylo_datapacks.energy_manipulation.item.ModItems;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Arm;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HeldItemRenderer.class)
-public class HeldItemRendererMixin {
+public abstract class HeldItemRendererMixin {
 
-    /**@Invoker("getUsingItemHandRenderType")
-    private static HandRenderType invokeGetUsingItemHandRenderType(ClientPlayerEntity player) {
-        throw new AssertionError();
+    @Shadow public abstract void applyEquipOffset(MatrixStack matrices, Arm arm, float equipProgress);
+
+    @Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
+    private void renderFirstPersonItemInject(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        if (item.isOf(ModItems.SPELL_BOOK)) {
+            boolean bl = hand == Hand.MAIN_HAND;
+            Arm arm = bl ? player.getMainArm() : player.getMainArm().getOpposite();
+            boolean bl3 = arm == Arm.RIGHT;
+            int i = bl3 ? 1 : -1;
+            if (player.isUsingItem() && player.getItemUseTimeLeft() > 0 && player.getActiveHand() == hand) {
+                this.applyEquipOffset(matrices, arm, equipProgress);
+                matrices.translate((float) i * -0.4785682F, -0.094387F, 0.05731531F);
+            }
+        }
     }
     
-    @Inject(method = "getHandRenderType", at = @At("HEAD"), cancellable = true)
-    private static void getHandRenderType(ClientPlayerEntity player, CallbackInfoReturnable<HandRenderType> cir) {
-        cir.setReturnValue(HeldItemRenderer.invokeGetUsingItemHandRenderType(player));
-    }*/
 }
